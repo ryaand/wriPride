243d79d47a12b748cf2bb448ec294e7ede63763c
Bahriansyah Herlambang <bahriansyah3@outlook.com>
2026-09-14 20:30:05 +0700

    feat: implement password validation loop in perulangan3 class

--- perulangan3.java
+++ perulangan3.java
@@ -3,9 +3,21 @@ import java.util.Scanner;
 public class perulangan3 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    boolean loop = true;
+    String passDefault = "1234";
 
-    while () {
+    while (loop) {
+      System.out.print("Masukkan password: ");
+      String passInput = sc.nextLine();
       
+      if (passInput.equals(passDefault)) {
+        System.out.println("Password benar!");
+        loop = false;
+      } else {
+        System.out.println("Password salah!");
+      }
     }
+
+    sc.close();
   }
 }
